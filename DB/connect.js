5790b2b8ec9deccc6d2a902cf5e0d8b822a0ea5f
renjith-irohub <renjith.ps@irohub.com>
2025-05-16 10:43:44 +0530

    changes added

--- DB/connect.js
+++ DB/connect.js
@@ -1,7 +1,7 @@
 const mongoose= require('mongoose');
 
 async function connect() {
-    await mongoose.connect("mongodb+srv://lakshmi:hRJRGMH2YeOH3tb9@cluster0.u8wkp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+    await mongoose.connect(process.env.MongoURI);
     console.log("mongodb connected , Welcome");
     
 }
